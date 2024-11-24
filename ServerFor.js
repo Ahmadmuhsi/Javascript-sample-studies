9f172309a3f6da6f93dd3a6d8be017d65a1c4650
ahmad muhsi <hameedrenja3456@gmail.com>
2024-11-24 16:37:04 +0530

    server with node js

--- ServerFor.js
+++ ServerFor.js
@@ -1,16 +1,48 @@
 var http = require('http')
 var fs = require('fs')
+var url= require('url')
 
 
-http.createServer(server).listen(7002)
+http.createServer(server).listen(7002,()=>
+    console.log("Server started")
+)
 
 
 function server(req, res) {
-    fs.readFile('Sample.html', function (err, data) {
+
+    var q=url.parse(req.url,true)
+   // console.log(q.pathname)
+
+
+
+    if(q.pathname==='/'){
+
+    
+    fs.readFile('Sample.html', function (err,data) {
         res.writeHead(200, { 'content-type': 'text/html' })
         res.write(data)
         res.end()
     })
+}else if(q.pathname==='/signup'){
+    fs.readFile('signup.html',(err,data)=>{
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write(data)
+        res.end()
+    })
+    
+}else if(q.pathname==='/signupaction'){
+    //console.log(q.query.lname+' '+q.query.fname)
+    res.writeHead(200, { 'content-type': 'text/html' })
+   res.write('<h1>'+q.query.fname+' '+q.query.lname+'</h1>')
+   res.end()
+
+
+}else{
+    res.writeHead(404,{'content-type':'text/html'})
+    res.write('error')
+    res.end()
+}
+
 }
 
 
